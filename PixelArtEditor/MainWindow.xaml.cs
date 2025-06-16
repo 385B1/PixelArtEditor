6a90743cc5744fea97bd241fa1968e7eec4b22e4
385B1 <A385B1@gmail.com>
2025-06-16 20:12:35 +0200

    Canvas of fixed size and pixel line separators

--- PixelArtEditor/MainWindow.xaml.cs
+++ PixelArtEditor/MainWindow.xaml.cs
@@ -16,9 +16,53 @@ namespace PixelArtEditor
     /// </summary>
     public partial class MainWindow : Window
     {
-        public MainWindow()
+
+        private int pixelSize = 20;
+        public MainWindow() // when the application starts
         {
             InitializeComponent();
+            DrawGrid();
+            // Console.WriteLine("Skibidi");
+        }
+
+        private void Canvas_MouseDown(object sender, MouseButtonEventArgs e) // when the mouse is clicked on the canvas
+        {
+
+        }
+
+
+        private void DrawGrid() {
+            
+            for (int x = 0; x < PixelCanvas.Width; x += pixelSize) // draw vertical lines
+            {
+                Line verticalLine = new Line
+                {
+                    X1 = x, // first point X coordinate
+                    Y1 = 0, // first point Y coordinate
+                    X2 = x, // second point X coordinate
+                    Y2 = PixelCanvas.Height, // second point Y coordinate
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 0.5
+
+                };
+                PixelCanvas.Children.Add(verticalLine); // add the line to the canvas
+
+            }
+
+            for (int y = 0; y < PixelCanvas.Height; y += pixelSize) // draw horizontal lines
+            {
+                Line horizontalLine = new Line
+                {
+                    X1 = 0,
+                    Y1 = y,
+                    X2 = PixelCanvas.Width,
+                    Y2 = y,
+                    Stroke = Brushes.Black,
+                    StrokeThickness = 0.5
+                };
+                PixelCanvas.Children.Add(horizontalLine);
+            }
+        
         }
     }
 }
