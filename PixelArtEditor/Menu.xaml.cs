bf957063a715ffec634b532d08d9dded14868d83
385B1 <A385B1@gmail.com>
2025-06-19 20:53:35 +0200

    FIle handling menu (not defined methods yet)

---
+++ PixelArtEditor/Menu.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PixelArtEditor
+{
+    public partial class MainWindow
+    {
+        private void New_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close(); // Closes the app
+        }
+    }
+}
